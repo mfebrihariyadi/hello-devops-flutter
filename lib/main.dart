6ed215c79e90c9bb4b022cb6bd6ba8170566c5f9
mfebrihariyadi <febrihariyadi@gmail.com>
2025-12-17 08:58:47 +0700

    Init Flutter

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('Hello World v3 - DevOps Local', style: TextStyle(fontSize: 32))),
+      ),
+    );
+  }
+}
